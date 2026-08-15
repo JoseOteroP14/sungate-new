14bd90f1a44533155569c120e46f5f5eea76c247
Jose Enrique Otero Pineda <enriquejo2002@gmail.com>
2026-08-15 13:00:16 -0500

    Add esbuild dependency and configure CSS minification in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Lightning CSS drops unprefixed backdrop-filter when the -webkit- prefix is also present.
+  // https://github.com/vitejs/vite/issues/22649
+  build: {
+    cssMinify: 'esbuild',
+  },
   optimizeDeps: {
     include: ['pmtiles', '@protomaps/basemaps'],
     exclude: ['maplibre-gl'],
